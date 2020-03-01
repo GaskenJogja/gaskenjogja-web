fb9df5c84cf9383bc5e135657dfb6f2dc2c6b098
arief <hariefv@gmail.com>
2020-03-01 09:21:25 +0700

    menambahkan file api-web

---
+++ api-web/api-firebase/User.php
@@ -0,0 +1,62 @@
+<?php
+
+require_once './vendor/autoload.php';
+
+use Kreait\Firebase\Factory;
+use Kreait\Firebase\ServiceAccount;
+
+class User {
+    protected $database;
+    protected $dbname = "users";
+
+    public function __construct(){
+        $acc = ServiceAccount::fromJsonFile(__DIR__.'/secret/gaskenjogja-9879d41e0f3f.json');
+        $firebase = (new Factory)->withServiceAccount($acc)->create();
+
+        $this->database = $firebase->getDatabase();
+    }
+
+    public function get(int $userId){
+        if(empty($userId) || !isset($userId)){ return false;}
+
+        if($this->database->getReference($this->dbname)->getSnapshot()->hasChild($userId)){
+            return $this->database->getReference($this->dbname)->getChild($userId)->getValue();
+        }
+        else{
+            return false;
+        }
+    }
+
+    public function insert(array $data){
+        if(empty($data) || !isset($data)){ return false;}
+
+        foreach ($data as $key => $val){
+            $this->database->getReference($this->dbname)->getChild($key)->set($val);
+        }
+
+        return true;
+    }
+
+    public function delete(int $userId){
+        if(empty($userId) || !isset($userId)){ return false;}
+
+        if($this->database->getReference($this->dbname)->getSnapshot()->hasChild($userId)){
+            $this->database->getReference($this->dbname)->getChild($userId)->remove();
+
+            return true;
+        }
+        else{
+            return false;
+        }
+
+    }
+}
+
+$user = new User();
+
+// var_dump($user->insert([
+//     "1" => "john",
+//     "2" => "asu"
+// ]));
+
+var_dump($user->delete(2));
